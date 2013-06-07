8ddf836177e718b43da0c7c9ff3c93f0adad81e5
Adam <a.sanchez75@gmail.com>
2013-06-07 12:29:28 -0500

    autocomplete widget works

--- theme/openlayers-geocoder-result.tpl.php
+++ theme/openlayers-geocoder-result.tpl.php
@@ -5,5 +5,5 @@
  * Template file theming geocoder's response results.
  */
 ?>
-<span class="openlayers-geocoder-result detail-row-1"><?php print $result['street_address']; ?></span>
-<span class="openlayers-geocoder-result detail-row-2"><?php print $result['postal_code'] .' '. $result['locality']; ?><?php print $result['locality'] ? ' - ' : ''; ?><?php print $result['country']; ?></span>
+<span class="openlayers-geocoder-result detail-row-1"><?php print $street_address; ?></span>
+<span class="openlayers-geocoder-result detail-row-2"><?php print $postal_code .' '. $locality; ?><?php print $locality ? ' - ' : ''; ?><?php print $country; ?></span>

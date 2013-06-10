4252581522e9c095127c871e1f0b74f3ca8d4aae
Adam <a.sanchez75@gmail.com>
2013-06-09 23:02:08 -0500

    json response works

--- js/openlayers_geocoder.js
+++ js/openlayers_geocoder.js
@@ -46,24 +46,24 @@ Drupal.Geocoder.prototype.process = function (query) {
   var formid = $("input[name=form_id]").val();
   var contenttype = formid.replace("_node_form", "");
 
-// console.log(fieldname);
-// console.log(dashed);
-// console.log(formid);
-// console.log(contenttype);
+ //console.log(fieldname);
+ //console.log(dashed);
+ //console.log(formid);
+ //console.log(contenttype);
   var data = {
     query:query,
     fieldname:fieldname,
     content_type:contenttype
   };
 //console.log(data);
-console.log(this);
+// console.log(this);
   $.ajax({
     type: 'POST',
     url: this.data.db.uri + '/process',
     data: data,
     dataType: 'json',
     success: function(point) {
-
+    console.log(point);
       if (point.longitude && point.latitude) {
 
         var data = $('#openlayers-cck-widget-map-' + fieldname).data('openlayers');
@@ -101,43 +101,6 @@ console.log(this);
   });
 
 }
-
-Drupal.behaviors.openlayerFieldWktHide.attach = function(context, settings) {
-  // Hide WKT field and allow to toggle visibility.
-  if (typeof Drupal.settings.openlayers_cck.wkt_hide != 'undefined') {
-    var textShow = Drupal.settings.openlayers_cck.wkt_hide.text_show;
-    var textHide = Drupal.settings.openlayers_cck.wkt_hide.text_hide;
-
-    for (field in Drupal.settings.openlayers_cck.wkt_hide.fields) {
-      var fieldID = '#edit-' + field + '-openlayers-wkt-wrapper';
-
-      $(fieldID + ':not(.openlayers-cck-processed)').each(function() {
-        var $thisField = $(this);
-
-        $thisField.addClass('openlayers-cck-processed');
-        // Add a link to be able to hide and show, and hide by default.
-        var link = $('<a href="" id="' + field + '-toggle" class="openlayers-cck-hide-link">' + textShow + '</a>')
-          .data('fieldID', fieldID);
-        $thisField.before(link)
-          .hide();
-        $('#' + field + '-toggle').toggle(
-          function () {
-            var $thisLink = $(this);
-            var localFieldID = $thisLink.data('fieldID');
-            $(localFieldID).slideDown();
-            $thisLink.text(textHide);
-          },
-          function () {
-            var $thisLink = $(this);
-            var localFieldID = $thisLink.data('fieldID');
-            $(localFieldID).slideUp();
-            $thisLink.text(textShow);
-          }
-        );
-      });
-    }
-  }
-};
 
 })(jQuery);
 
